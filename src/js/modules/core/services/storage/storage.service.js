e6873dc5a739054eef847043b63b4c35ffb90dee
Tobias Hoffmann <lacksfish@gmail.com>
2018-05-11 15:13:34 +0200

    bump pouchDB version, try some things, wip

--- src/js/modules/core/services/storage/storage.service.js
+++ src/js/modules/core/services/storage/storage.service.js
@@ -29,7 +29,9 @@ angular.module('blocktrail.wallet').factory(
             if (!dbs[name]) {
                 dbs[name] = new PouchDB(name, {
                     adapter: CONFIG.POUCHDB_DRIVER,
-                    location: 2 // storage file on iOS in location that is not synced to iTunes or iCloud
+                    // location: 2, // storage file on iOS in location that is not synced to iTunes or iCloud
+                    iosDatabaseLocation: 2,
+                    androidDatabaseImplementation: 2
                 });
 
                 dbs[name].on('error', function(err) {
